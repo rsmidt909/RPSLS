432481d1434012e8fff889212ff4a080f0811a9e
Robert Smidt <rsmidt909@hotmail.com>
2019-05-01 14:32:34 -0500

    Fixed DoBattle method to no longer push ConsoleWrite, but to just return a string. Added in variable to catch return called roundWinner

--- RPSLS/RPSLS/Battlefield.cs
+++ RPSLS/RPSLS/Battlefield.cs
@@ -32,133 +32,134 @@ namespace RPSLS
         //member methods(CAN DO)
         public string DoBattle()
         { if (player1.wep == "Rock" && player2.wep == "Rock")
-            {
-                Console.WriteLine("Tie!");
+            {               
+               return "Tie!";
+
             } else if (player1.wep == "rock" && player2.wep == "Paper")
             {
-                Console.WriteLine("Player 2 wins!");
+                return "Player 2 wins!";
                     
             }else if (player1.wep == "rock" && player2.wep == "Scissors")
             {
-                Console.WriteLine("Player 1 wins!");
+                return "Player 1 wins!";
                     
             } else if (player1.wep == "Rock" && player2.wep == "Lizard")
             {
-                Console.WriteLine("Player 1 wins!");
+                return "Player 1 wins!";
                     
             } else if (player1.wep == "Rock" && player2.wep == "Spock")
             {
-                Console.WriteLine("Player2  wins!");
+                return "Player2  wins!";
                     
             }
             //-------------------------------------------------------------
             else if (player1.wep == "Paper" && player2.wep == "Paper")
             {
-                Console.WriteLine("Tie!");
+                return "Tie!";
             }
             else if (player1.wep == "Paper" && player2.wep == "Rock")
             {
-                Console.WriteLine("Player 1 wins!");
+                return "Player 1 wins!";
                     
             }
             else if (player1.wep == "Paper" && player2.wep == "Scissors")
             {
-                Console.WriteLine("Player 2 wins!");
+                return "Player 2 wins!";
 
             }
             else if (player1.wep == "Paper" && player2.wep == "Lizard")
             {
-                Console.WriteLine("Player 2 wins!");
+                return "Player 2 wins!";
                     
             }
             else if (player1.wep == "Paper" && player2.wep == "Spock")
             {
-                Console.WriteLine("Player 1  wins!");
+                return "Player 1  wins!";
                     
             }
             //-----------------------------------------------------------------
             else if (player1.wep == "Scissors" && player2.wep == "Scissors")
             {
-                Console.WriteLine("Tie!");
+                return "Tie!";
             }
             else if (player1.wep == "Sissors" && player2.wep == "Rock")
             {
-                Console.WriteLine("Player 2 wins!");
+                return "Player 2 wins!";
 
 
             }
             else if (player1.wep == "Scissors" && player2.wep == "Paper")
             {
-                Console.WriteLine("Player 1 wins!");
+                return "Player 1 wins!";
 
             }
             else if (player1.wep == "Scissors" && player2.wep == "Lizard")
             {
-                Console.WriteLine("Player 1 wins!");
+                return "Player 1 wins!";
 
             }
             else if (player1.wep == "Scissors" && player2.wep == "Spock")
             {
-                Console.WriteLine("Player 2  wins!");
+                return "Player 2  wins!";
 
 
             }
             //-----------------------------------------------------------------------------
             else if (player1.wep == "Lizard" && player2.wep == "Lizard")
             {
-                Console.WriteLine("Tie!");
+                return "Tie!";
 
             }
             else if (player1.wep == "Lizard" && player2.wep == "Rock")
             {
-                Console.WriteLine("Player 2 wins!");
+                return "Player 2 wins!";
 
             }
             else if (player1.wep == "Lizard" && player2.wep == "Paper")
             {
-                Console.WriteLine("Player 1 wins!");
+                return "Player 1 wins!";
 
             }
             else if (player1.wep == "Lizard" && player2.wep == "Scissors")
             {
-                Console.WriteLine("Player 2 wins!");
+                return "Player 2 wins!";
 
             }
             else if (player1.wep == "Lizard" && player2.wep == "Spock")
             {
-                Console.WriteLine("Player 1  wins!");
+                return "Player 1  wins!";
 
 
             }
             //-------------------------------------------------------------------
             else if (player1.wep == "Spock" && player2.wep == "Spock")
             {
-                Console.WriteLine("Tie!");
+                return "Tie!";
 
             }
             else if (player1.wep == "Spock" && player2.wep == "Rock")
             {
-                Console.WriteLine("Player 1 wins!");
+                return "Player 1 wins!";
 
             }
             else if (player1.wep == "Spock" && player2.wep == "Paper")
             {
-                Console.WriteLine("Player 2 wins!");
+                return "Player 2 wins!";
 
             }
             else if (player1.wep == "Spock" && player2.wep == "Scissors")
             {
-                Console.WriteLine("Player 1 wins!");
+                return "Player 1 wins!";
 
             }
             else if (player1.wep == "Spock" && player2.wep == "Lizard")
             {
-                Console.WriteLine("Player 2  wins!");
+                return "Player 2  wins!";
 
 
             } else
             {
-                Console.WriteLine("Something went wrong....");
+                return "Something went wrong....";
             };
         }   
 
